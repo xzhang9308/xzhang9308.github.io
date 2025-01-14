cbd9b8406e6e3bafb0b6649cdfbe3c0e3987e1c1
xzhang9308 <xzhang9308@gmail.com>
2025-01-14 13:07:12 +0000

    Deploying to gh-pages from @ xzhang9308/xzhang9308.github.io@665595c7c71ecc6740e05e465d21cda2a5892099 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,16 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
