aad956fa008713cb32cc5dd1304bee98193ed47c
Xi Zhang <xzhang9308@gmail.com>
2026-06-26 13:17:02 +0800

    Add analytics visitor label setup link

--- assets/js/homepage-analytics.js
+++ assets/js/homepage-analytics.js
@@ -2,11 +2,26 @@
   var endpoint = window.homepageAnalyticsEndpoint;
   if (!endpoint) return;
 
+  var visitorLabel = "";
+  try {
+    var search = new URLSearchParams(window.location.search);
+    var hash = new URLSearchParams(window.location.hash.replace(/^#/, ""));
+    visitorLabel = search.get("analytics_visitor") || hash.get("analytics_visitor") || "";
+
+    if (visitorLabel) {
+      localStorage.setItem("homepageAnalyticsVisitorLabel", visitorLabel);
+    } else {
+      visitorLabel = localStorage.getItem("homepageAnalyticsVisitorLabel") || "";
+    }
+  } catch (error) {
+    visitorLabel = "";
+  }
+
   var params = new URLSearchParams();
   params.set("page", window.location.pathname + window.location.search);
   params.set("title", document.title);
   params.set("referrer", document.referrer);
-  params.set("visitor_label", localStorage.getItem("homepageAnalyticsVisitorLabel") || "");
+  params.set("visitor_label", visitorLabel);
   params.set("t", String(Date.now()));
 
   var image = new Image(1, 1);
